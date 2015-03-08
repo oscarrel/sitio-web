0f2b3e514fe0cdc545545a9a2a6476b99f664c56
oscarrel <oscarrela@yahoo.es>
2015-03-08 01:19:11 +0100

    Completada galeria y añadido main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,36 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+(function () {
+'use strict';
+$(function() {
+
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Floating label headings for the contact form
+$(function() {
+    $('body').on('input propertychange', '.floating-label-form-group', function(e) {
+        $(this).toggleClass('floating-label-form-group-with-value', !! $(e.target).val());
+    }).on('focus', '.floating-label-form-group', function() {
+        $(this).addClass('floating-label-form-group-with-focus');
+    }).on('blur', '.floating-label-form-group', function() {
+        $(this).removeClass('floating-label-form-group-with-focus');
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Cierra el Menu responsivo del menu de items
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+}());
